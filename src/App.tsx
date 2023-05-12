6e51603b3e2305d88fc644438e6d1c3fbdad439a
Janett Gerrard <mikanchik@gmail.com>
2023-05-12 18:13:14 +0100

    add template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { HexGrid, Layout, Path, Text, Hexagon, HexUtils, GridGenerator } from 'react-hexgrid';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [hexagons, setHexagons] = useState(GridGenerator.hexagon(4));
+  const [path, setPath] = useState({ start: null, end: null });
+
+  const onClick = (event, source) => {
+    if (path.start === null) {
+      setPath({ start: source.state.hex, end: null });
+    } else {
+      setPath({ start: null, end: null });
+    }
+  };
+
+  const onMouseEnter = (event, source) => {
+    const targetHex = source.state.hex;
+
+    // Color some hexagons
+    const coloredHexagons = hexagons.map(hex => {
+      hex.props = hex.props || {};
+      // Highlight tiles that are next to the target (1 distance away)
+      hex.props.className = HexUtils.distance(targetHex, hex) < 2 ? 'active' : '';
+
+      // If the tile is on the same coordinate, add class specific to the coordinate name
+      hex.props.className += targetHex.q === hex.q ? ' q ' : '';
+      hex.props.className += targetHex.r === hex.r ? ' r ' : '';
+      hex.props.className += targetHex.s === hex.s ? ' s ' : '';
+
+      return hex;
+    });
+
+    setPath({ start: path.start, end: targetHex });
+    setHexagons(coloredHexagons);
+  };
+
+  const renderedhexes = hexagons.map((hex, i) => {
+    return (
+    <Hexagon
+      key={i}
+      q={hex.q}
+      r={hex.r}
+      s={hex.s}
+      className={hex.props ? hex.props.className : null}
+      onMouseEnter={(e, h) => onMouseEnter(e, h)}
+      onClick={(e, h) => onClick(e, h)}
+    >
+      <Text>{HexUtils.getID(hex)}</Text>
+    </Hexagon>
+  )})
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <h2>Pathfinding & active highlight</h2>
+      <p>Click a tile to start drawing a path to your cursor. Click again to cancel.</p>
+      <p>Hover around the board to see helper lines drawn.</p>
+      <HexGrid width={1200} height={800}>
+        <Layout size={{ x: 6, y: 6 }} flat={false} spacing={1.1} origin={{ x: 0, y: 0 }}>
+          { renderedhexes }
+          <Path start={path.start} end={path.end} />
+        </Layout>
+      </HexGrid>
+    </div>
+  );
+};
 
-export default App
+export default App;
