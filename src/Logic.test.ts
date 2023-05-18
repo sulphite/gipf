8423d26bad1eb9f6060a239ad4b8d4982de9b193
Janett Gerrard <mikanchik@gmail.com>
2023-05-18 16:03:56 +0100

    create getValidNeighbors function

--- src/Logic.test.ts
+++ src/Logic.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "vitest"
 import * as Logic from "./Logic"
 
+const testCoord = {q: 1, r: 0, s: -1}
 
 test("isClickable function exists", () => {
   // console.log(typeof(Logic.isClickable))
@@ -10,4 +11,8 @@ test("isClickable function exists", () => {
 test("isClickable returns boolean", () => {
   expect(Logic.isClickable({q: 4, r: -1, s: -3})).toBe(true);
   expect(Logic.isClickable({q: -3, r: 0, s: 3})).toBe(false);
+ })
+
+ test("getvalidNeighbors returns something", () => {
+  expect(Logic.getValidNeighbors(testCoord)).toBeDefined()
  })
--- src/Logic.tsx
+++ src/Logic.tsx
@@ -5,3 +5,8 @@ import { HexCoordinates } from 'react-hexgrid/lib/models/Hex';
 export const isClickable = (hex: HexCoordinates) => {
   return HexUtils.lengths(hex) === 4
 }
+
+export const getValidNeighbors = (hex: HexCoordinates) => {
+  let neighbors = HexUtils.neighbors(hex)
+  return neighbors
+}
