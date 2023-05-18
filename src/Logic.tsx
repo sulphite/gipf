a52b39ea082e47589f34bcbf11078e1b863b7ba0
Janett Gerrard <mikanchik@gmail.com>
2023-05-18 15:01:46 +0100

    create isClickable function

---
+++ src/Logic.tsx
@@ -0,0 +1,6 @@
+import { HexUtils } from 'react-hexgrid';
+import { HexCoordinates } from 'react-hexgrid/lib/models/Hex';
+
+export const isClickable = (hex: HexCoordinates) => {
+  return HexUtils.distance(hex, {q: 0, r: 0, s: 0})
+}
