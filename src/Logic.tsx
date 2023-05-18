b4565678d85a5f9cb5cbe735750091cb75180b13
Janett Gerrard <mikanchik@gmail.com>
2023-05-18 15:24:08 +0100

    refactor

--- src/Logic.tsx
+++ src/Logic.tsx
@@ -1,6 +1,10 @@
 import { HexUtils } from 'react-hexgrid';
 import { HexCoordinates } from 'react-hexgrid/lib/models/Hex';
 
+const distFromOrigin = (hex: HexCoordinates) => {
+  return HexUtils.distance(hex, {q: 0, r: 0, s: 0})
+}
+
 export const isClickable = (hex: HexCoordinates) => {
-  return HexUtils.distance(hex, {q: 0, r: 0, s: 0}) > 3
+  return distFromOrigin(hex) === 4
 }
