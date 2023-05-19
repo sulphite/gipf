771628f1355d400edc92df97eed0332f02a6b9c8
Janett Gerrard <mikanchik@gmail.com>
2023-05-19 13:01:25 +0100

    render from state data

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,24 @@ const initializeGrid = () => {
 }
 
 const App = () => {
+  // const hexagons = GridGenerator.hexagon(4)
   const [hexagons, setHexagons] = useState(initializeGrid());
   const [path, setPath] = useState({ start: null, end: null });
+  const [hexagonData, setHexagonData] = useState(hexagons.map(hex => {
+    return {
+      q: hex.q,
+      r: hex.r,
+      s: hex.s,
+      id: HexUtils.getID({q: hex.q,r: hex.r,s: hex.s}),
+      data: {status: "" },
+      className: HexUtils.lengths({q: hex.q,r: hex.r,s: hex.s}) === 4 ? "outer" : ""
+    }
+  }));
   // const [gameState, setGameState] = useState({black: 15, white: 15});
   // const [currentPlayerWhite, setCurrentPlayerWhite] = useState(true);
 
-
   const onClick = (_event: any, source: { data?: any; state: any; props?: HexagonProps; }) => {
-    console.log(source.state.hex.props)
+    // console.log(source.state.hex.props)
     // check if hex is clickable
     if(Logic.isClickable(source.state.hex)) {
       //colour pushable rows
@@ -29,7 +39,6 @@ const App = () => {
         hex.props = hex.props || {};
 
         if (Logic.hexIncludes(pushable,hex)) {
-          console.log(true)
           hex.props.className += " pushable"
         }
 
@@ -49,14 +58,14 @@ const App = () => {
     const targetHex = source.state.hex;
 
     // Color some hexagons
-    const coloredHexagons = hexagons.map(hex => {
-      hex.props = hex.props || {};
+    const coloredHexagons = hexagonData.map(hex => {
+
 
       //highlight outer ring
-      hex.props.className = Logic.isClickable(hex) ? "outer" : ""
+      // hex.props.className = Logic.isClickable(hex) ? "outer" : ""
 
       // Highlight tiles that are next to the target (1 distance away)
-      hex.props.className += HexUtils.distance(targetHex, hex) < 1 ? ' active ' : '';
+      // hex.props.className += HexUtils.distance(targetHex, hex) < 1 ? ' active ' : '';
       // If the tile is on the same coordinate, add class specific to the coordinate name
       // hex.props.className += targetHex.q === hex.q ? ' q ' : '';
       // hex.props.className += targetHex.r === hex.r ? ' r ' : '';
@@ -66,21 +75,23 @@ const App = () => {
     });
 
     setPath({ start: path.start, end: targetHex });
-    setHexagons(coloredHexagons);
+    setHexagonData(coloredHexagons);
   };
 
-  const renderedhexes = hexagons.map((hex, i) => {
+  const renderedhexes = hexagonData.map((hex) => {
+    console.log(hex)
     return (
     <Hexagon
-      key={i}
+      key={hex.id}
       q={hex.q}
       r={hex.r}
       s={hex.s}
-      className={hex.props ? hex.props.className : null}
+      data={hex.data}
+      className={hex.className}
       onMouseEnter={(e, h) => onMouseEnter(e, h)}
       onClick={(e, h) => onClick(e, h)}
     >
-      <Text>{HexUtils.getID(hex)}</Text>
+      <Text>{hex.id}</Text>
       {/* <circle className="black" r="3" /> */}
     </Hexagon>
   )})
