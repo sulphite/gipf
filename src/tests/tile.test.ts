e0a1ed27463d90a2993a8583c19fb47f893ca2ef
Adrian Hards <github@ahards.com>
2023-09-18 10:24:35 +0100

    lint

--- src/tests/tile.test.ts
+++ src/tests/tile.test.ts
@@ -3,23 +3,26 @@ import { Board } from "../logic/Board";
 const board = new Board();
 
 test("outer coord returns true with isOuterTile", () => {
-    const tile = board.grid.getHex([4, 0, -4]);
-    tile && typeof tile.isOuterTile === 'function'
-        ? expect(tile.isOuterTile()).toBe(true)
-        : fail("tile is undefined");
+  const tile = board.grid.getHex([4, 0, -4]);
+  tile && typeof tile.isOuterTile === "function"
+    ? expect(tile.isOuterTile()).toBe(true)
+    : fail("tile is undefined");
 });
 
 test("when I access the fill property it returns a string", () => {
-    const tile = board.grid.getHex([0, 0]);
-    tile && typeof tile.getFill === 'function'
-        ? expect(tile.getFill(board.grid)).toBe("")
-        : fail("tile is undefined");
+  const tile = board.grid.getHex([0, 0]);
+  tile && typeof tile.getFill === "function"
+    ? expect(tile.getFill(board.grid)).toBe("")
+    : fail("tile is undefined");
 });
 
 test("I can set the fill property of a tile", () => {
-    const tile = board.grid.getHex([0, 1, -1]);
+  const tile = board.grid.getHex([0, 1, -1]);
 
-    tile && typeof tile.setFill === 'function' && typeof tile.getFill === 'function'
-        ? (tile.setFill(board.grid, "W"), expect(tile.getFill(board.grid)).toBe("W"))
-        : fail("tile is undefined");
+  tile &&
+    typeof tile.setFill === "function" &&
+    typeof tile.getFill === "function"
+    ? (tile.setFill(board.grid, "W"),
+      expect(tile.getFill(board.grid)).toBe("W"))
+    : fail("tile is undefined");
 });
