3eeca1f29974deb32373a191f4366fd3f7650405
Janett Gerrard <mikanchik@gmail.com>
2023-10-17 16:22:40 +0100

    handle join event

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
+import { Lobby } from "./src/Lobby";
+
 const port = 3000;
+const lobby = new Lobby();
 
 const server = Bun.serve({
   port: port,
   websocket: {
     open(ws) {
       ws.subscribe("room");
-
-      // remainingClients--;
       console.log(`${ws.data.name} connected`);
+      lobby.addPlayer(ws.data.name, ws);
+
+      console.log(`${Object.keys(lobby.players).length} players are here`)
 
       // if (remainingClients === 0) {
       //   console.log("All clients connected");
@@ -17,7 +21,16 @@ const server = Bun.serve({
       //   }, 100);
       // }
     },
-    message(ws, msg) {
+    message(ws, msg: string) {
+      const message = JSON.parse(msg);
+
+      // join event
+      if(message.type == "join") {
+        let room = lobby.getOpenRoom();
+        room.addSocket(ws);
+        console.log(`added ${message.name} to room ${room.id}`)
+      }
+
       const out = `${ws.data.name}: ${msg}`;
       console.log(out)
       if (ws.publishText("room", out) !== out.length) {
@@ -36,7 +49,7 @@ const server = Bun.serve({
     if (
       server.upgrade(req, {
         data: {
-          name: new URL(req.url).searchParams.get("name") || "Client #",
+          name: new URL(req.url).searchParams.get("name") || "ClientName",
         },
       })
     )
