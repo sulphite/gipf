1126f7096075e99776906b34a5ad1ee76dc0952b
Janett Gerrard <mikanchik@gmail.com>
2023-10-23 13:46:19 +0100

    subscribe and unsubscribe sockets from lobby updates appropriately

--- index.ts
+++ index.ts
@@ -8,19 +8,11 @@ const server = Bun.serve<{name: string;}>({
   port: port,
   websocket: {
     open(ws) {
-      ws.subscribe("room");
-      console.log(`${ws.data.name} connected`);
       lobby.addPlayer(ws.data.name, ws);
-
-      console.log(`${Object.keys(lobby.players).length} players are here`)
-
-      // if (remainingClients === 0) {
-      //   console.log("All clients connected");
-      //   setTimeout(() => {
-      //     console.log('Starting benchmark by sending "ready" message');
-      //     ws.publishText("room", `ready`);
-      //   }, 100);
-      // }
+      ws.subscribe("lobby")
+      let connectionMessage = JSON.stringify({type: "lobby", data:`${ws.data.name} connected. ${Object.keys(lobby.players).length} players are here`})
+      console.log(connectionMessage);
+      ws.publishText("lobby", connectionMessage)
     },
     message(ws, msg: string) {
       const message = JSON.parse(msg);
@@ -29,6 +21,8 @@ const server = Bun.serve<{name: string;}>({
       if(message.type == "join") {
         let room = lobby.getOpenRoom();
         room.addSocket(ws);
+        ws.unsubscribe("lobby");
+        ws.subscribe(room.id);
         console.log(`added ${message.name} to room ${room.id}`)
         ws.send(JSON.stringify({ type: "roomJoined", room: room.id }))
       }
