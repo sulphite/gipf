115de89bbcdb430d2018e917db88952525a81ee6
Janett Gerrard <mikanchik@gmail.com>
2024-01-18 17:39:29 +0000

    remove commented code

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -11,15 +11,6 @@ export type HexagonProps = {
 
 export const Board = ({ hexes } ) => {
 
-  // const Hex = defineHex({ dimensions: 50, origin: 'topLeft' })
-  // const grid = new Grid(Hex, spiral({radius: 3}))
-
-  // const hexagondata: HexagonProps[] = []
-
-  // grid.forEach((hex) => {
-  //   hexagondata.push({coords: hex.toString(), points: hex.corners, center: hex.center, piece: "B", isOuter: true})
-  // })
-
   const hexdata: HexagonProps[] = hexes.map(hex => {
     return {
       coords: JSON.stringify({q: hex.q, r: hex.r}),
