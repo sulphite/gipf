76ec27b49252ba5a5c6f4364f073b64ff1d8d8dc
Janett Gerrard <mikanchik@gmail.com>
2024-01-18 17:36:28 +0000

    use data from props to render board

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,4 +1,4 @@
-import { defineHex, Grid, spiral, Point } from 'honeycomb-grid'
+import { Point } from 'honeycomb-grid'
 import { Hexagon } from "./Hexagon"
 
 export type HexagonProps = {
@@ -6,29 +6,39 @@ export type HexagonProps = {
   points: Point[];
   piece?: string;
   isOuter: boolean;
+  center?: Point;
 }
 
-export const Board = () => {
+export const Board = ({ hexes } ) => {
 
-  const Hex = defineHex({ dimensions: 50, origin: 'topLeft' })
-  const grid = new Grid(Hex, spiral({radius: 3}))
+  // const Hex = defineHex({ dimensions: 50, origin: 'topLeft' })
+  // const grid = new Grid(Hex, spiral({radius: 3}))
 
-  const hexagondata: HexagonProps[] = []
+  // const hexagondata: HexagonProps[] = []
 
-  grid.forEach((hex) => {
-    hexagondata.push({coords: hex.toString(), points: hex.corners, piece: "B", isOuter: true})
+  // grid.forEach((hex) => {
+  //   hexagondata.push({coords: hex.toString(), points: hex.corners, center: hex.center, piece: "B", isOuter: true})
+  // })
+
+  const hexdata: HexagonProps[] = hexes.map(hex => {
+    return {
+      coords: JSON.stringify({q: hex.q, r: hex.r}),
+      points: hex.corners,
+      piece: hex.fill,
+      isOuter: true
+    }
   })
 
   return (<svg
   className="grid"
   width="800"
-  height="600"
+  height="800"
   viewBox="-350 -250 800 600"
   version="1.1"
   xmlns="http://www.w3.org/2000/svg"
 >
   <g>
-    {hexagondata.map((h) => <Hexagon data={h} />)}
+    {hexdata.map((h) => <Hexagon data={h} />)}
   </g>
 </svg>)
 }
